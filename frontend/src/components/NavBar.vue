e49d156240451d90c714f301e855993b223e5dab
Siarhei Ivanouski <siarhei.ivanouski@xbav.de>
2019-11-26 09:33:45 +0100

    Restore deleted by mistake shared components

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app-bar
+    app
+    dark
+  >
+    <v-toolbar-title>Bankoque</v-toolbar-title>
+    <v-spacer />
+    <v-btn
+      :to="{ name: 'Users' }"
+      color="teal"
+      primary
+      dark
+    >Users</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+};
+</script>
---
+++ frontend/src/components/Notifications.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-snackbar
+    :value="error || message"
+    :color="error ? 'error' : 'success'"
+    multi-line
+    top
+  >{{ error || message }}</v-snackbar>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Notifications",
+
+  computed: {
+    ...mapGetters("client", ["error"]),
+    ...mapGetters("ui", ["message"])
+  }
+};
+</script>
+
+<style scoped>
+</style>
