45998c9da263ebd5f879b41eb57184a3f931d3b9
Siarhei Ivanouski <mafflin@gmail.com>
2021-01-01 18:34:06 +0100

    Add n26 parser

---
+++ app/services/reports/n26_parse_service.rb
@@ -0,0 +1,56 @@
+require 'csv'
+
+module Reports
+  class N26ParseService < ApplicationService
+    COL_SEP = ','
+    ENCODING = 'UTF-8'
+    DATE_FORMAT = '%Y-%m-%d'
+
+    def initialize(report)
+      @report = report
+    end
+
+    def perform
+      CSV.parse(@report, **params).map { |line| parse(line.to_hash) }
+    end
+
+    private
+
+    def parse(entry)
+      original_amount = entry['Amount (EUR)']
+      amount = parse_amount(original_amount)
+      is_debit = is_debit?(amount)
+
+      {
+        booked_at: Date.strptime(entry['Date'], DATE_FORMAT),
+        transaction_type: entry['Transaction type'],
+        details: entry['Category'],
+        beneficiary: entry['Payee'],
+        customer_reference: entry['Payment reference'],
+        mandate_reference: nil,
+        external_creditor_id: nil,
+        currency: entry['Type Foreign Currency'],
+        original_debit: is_debit && original_amount,
+        original_credit: !is_debit && original_amount,
+        debit: is_debit ? amount : 0,
+        credit: !is_debit ? amount : 0,
+      }
+    end
+
+    def params
+      @params ||= {
+        headers: true,
+        encoding: ENCODING,
+        col_sep: COL_SEP,
+      }
+    end
+
+    def parse_amount(value)
+      value&.to_f || 0
+    end
+
+    def is_debit?(value)
+      value < 0
+    end
+  end
+end
--- app/services/reports/parse_service.rb
+++ app/services/reports/parse_service.rb
@@ -2,49 +2,15 @@ require 'csv'
 
 module Reports
   class ParseService < ApplicationService
-    COL_SEP = ';'
-    ENCODING = 'UTF-8'
-    DATE_FORMAT = '%m/%d/%Y'
-    LINE_MATCH = /^((?!(\d\d\/\d\d\/\d\d\d\d;\d\d\/\d\d\/\d\d\d\d|Booking)).)*$/
+    DB_IDENTIFIER = 'Customer number:'
 
     def initialize(report)
-      @report = report
+      is_db = report.include?(DB_IDENTIFIER)
+      @service = is_db ? DbParseService.new(report) : N26ParseService.new(report)
     end
 
     def perform
-      CSV.parse(@report, **params).map { |line| parse(line.to_hash) }
-    end
-
-    private
-
-    def parse(entry)
-      {
-        booked_at: Date.strptime(entry['Booking date'], DATE_FORMAT),
-        transaction_type: entry['Transaction Type'],
-        details: entry['Payment Details'],
-        beneficiary: entry['Beneficiary / Originator'],
-        customer_reference: entry['Customer Reference'],
-        mandate_reference: entry['Mandate Reference'],
-        external_creditor_id: entry['Creditor ID'],
-        currency: entry['Currency'],
-        original_debit: entry['Debit'],
-        original_credit: entry['Credit'],
-        debit: parse_amount(entry['Debit']),
-        credit: parse_amount(entry['Credit']),
-      }
-    end
-
-    def params
-      @params ||= {
-        headers: true,
-        encoding: ENCODING,
-        col_sep: COL_SEP,
-        skip_lines: LINE_MATCH
-      }
-    end
-
-    def parse_amount(value)
-      value&.gsub(/[,]/, '')&.to_f || 0
+      @service.perform
     end
   end
 end
