3210afedf0b9f17146b60cc90dcb8595876aa147
Siarhei Ivanouski <siarhei.ivanouski@xempus.com>
2021-08-20 18:11:46 +0200

    Move geo functions to a concern

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,6 @@
 class Payment < ApplicationRecord
+  include GeoTraceable
+
   belongs_to :user
   belongs_to :location, optional: true
 
@@ -8,26 +10,11 @@ class Payment < ApplicationRecord
   validates :digest, presence: true, uniqueness: true
   validates :withdrawal, numericality: true
 
-  # TODO: move to DB.
-  GEOTRACEABLE_TYPES = [
-    'Debit Card Payment',
-    'MasterCard Payment',
-  ]
-  # TODO: move to DB.
-  NON_GEOTRACEABLE_MATCH = [
-    'account statement',
-    'amzn',
-    'amazon',
-    'google',
-    'income',
-    'onlin',
-    'paypal',
-  ]
   BANKS = {
     db: 'DB',
     n26: 'n26',
+    revolut: 'revolut',
   }
-  QUERY_SEPARATOR = '+'
 
   class << self
     def by_year(date)
@@ -53,27 +40,5 @@ class Payment < ApplicationRecord
         user.rules.map(&:match_string)
       )
     end
-  end
-
-  def geo_traceable?
-    GEOTRACEABLE_TYPES.include?(transaction_type) && !NON_GEOTRACEABLE_MATCH.any? do |word|
-      [beneficiary, details].join.downcase.include?(word)
-    end
-  end
-
-  def geo_query
-    return unless geo_traceable?
-
-    @geo_query ||= case bank
-    when Payment::BANKS[:n26]
-      beneficiary
-    when Payment::BANKS[:db]
-      details
-    end
-
-    @geo_query&.downcase
-      .gsub(/[^A-Za-z]/, ' ')
-      .split(' ')
-      .join(QUERY_SEPARATOR)
   end
 end
